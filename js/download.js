de6a5535f933ec3d36524ff20821a4a6e2d1beeb
Moelee <san.limeng@qq.com>
2020-07-30 23:29:22 +0800

    update

--- js/download.js
+++ js/download.js
@@ -1,4 +1,4 @@
-$.getJSON("json/data.json", function(data){
+$.getJSON("json/download.json", function(data){
     var dom = '';
     $.each(data, function(index, item){
         dom += '<li><font face="Microsoft Yahei">['+item.number+'] '+item.title+'</font></li>'
