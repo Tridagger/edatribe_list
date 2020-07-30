968dfb5e0d6a41c7e881dc03140f8be9182ef212
Moelee <san.limeng@qq.com>
2020-07-31 00:41:26 +0800

    update

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 $.getJSON("json/download.json", function(data){
     var dom = '';
     $.each(data, function(index, item){
-        dom += '<li><font face="Microsoft Yahei">['+item.number+'] '+item.title+'</font></li>'
+        dom += '<a href="'+item.link+'" target="view_frame"><li><font face="Microsoft Yahei">'+item.number+' 提取码：'+item.key+'</font></li></a>'
     });
     $('.mulu_list').html(dom);
 })
