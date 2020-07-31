e6e9836d98163d0ad7156129511e278a27ad1e8d
Tridagger <san.limeng@qq.com>
2020-07-31 18:53:29 +0800

    update

--- js/index.js
+++ js/index.js
@@ -4,25 +4,36 @@ function render_json_data(keyword = ''){
         var dom = '';
         $.each(data, function(index, item){
             // dom += '<li><font face="Microsoft Yahei">['+item.number+'] '+item.title+'</font></li>'
-            if(keyword.length === 0 || item.title.indexOf(keyword) !== -1 || item.number.indexOf(keyword) !== -1 ){
-                dom += getItem(item, keyword)
-            }
+            dom += getItem(item, keyword)
         });
         $('.mulu_list').html(dom);
     });
 }
 
-function getItem(item, keyword = '')
-{
+function getItem(item, keyword = '') {
     var number = item.number;
-    //高亮处理
-    number = number.split(keyword).join('<label style="color:#0f0;background-color:#ff0">'+keyword+'</label>');
-
     var title = item.title;
-    //高亮处理
-    title = title.split(keyword).join('<label style="color:#0f0;background-color:#ff0">'+keyword+'</label>');
+    if (keyword.length !== 0) {
+        var pattern = new RegExp(keyword, 'gmi');
+        if(!pattern.test(number+title)){
+            return '';
+        }
+        // 生成正则表达式
+        var keywordLower = keyword.toLowerCase();
+        var patternLower = new RegExp(keywordLower, 'gm');
+        //高亮处理
+        number = number.replace(patternLower, '<label style="color:#0f0;background-color:#ff0">' + keywordLower + '</label>');
+        title = title.replace(patternLower, '<label style="color:#0f0;background-color:#ff0">' + keywordLower + '</label>');
 
-    return '<li><font face="Microsoft Yahei">['+number+'] '+title+'</font></li>';
+        var keywordUp = keyword.toUpperCase();
+        var patternUp = new RegExp(keywordUp, 'gm');
+        //高亮处理
+        number = number.replace(patternUp, '<label style="color:#0f0;background-color:#ff0">' + keywordUp + '</label>');
+        title = title.replace(patternUp, '<label style="color:#0f0;background-color:#ff0">' + keywordUp + '</label>');
+
+
+    }
+    return '<li><font face="Microsoft Yahei">[' + number + '] ' + title + '</font></li>';
 }
 
 function init()
--- js/search.js
+++ js/search.js
@@ -1,3 +1,9 @@
 $('#search-button').click(function () {
     render_json_data($('#key-word').val());
 });
+
+$('#key-word').keydown(function(event){
+    if (event.keyCode==13){
+        render_json_data($('#key-word').val());
+    }
+})
